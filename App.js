9e6c6417d7a1863d8e7681cfffd401c147c5d2a7
vaibhav <vaibhavbh77@gmail.com>
2023-03-16 04:57:16 +0530

    todays changes 15 march

---
+++ App.js
@@ -0,0 +1,6 @@
+const heading = document.createElement("h1");
+heading.innerHTML = "this is the text inside it";
+
+const root = document.getElementById("root");
+root.appendChild(heading);
+console.log("new one");
