62c99160420f8a2dc928cb9913468ec5056446db
vaibhav <vaibhavbh77@gmail.com>
2023-04-16 00:10:46 +0530

    changes for 2 chapter

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
-const heading = document.createElement("h1");
-heading.innerHTML = "this is the text inside it";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = document.getElementById("root");
-root.appendChild(heading);
-console.log("new one");
+const heading1 = React.createElement("h1", { id: "title" }, "inside h1");
+
+const heading2 = React.createElement("h2", { id: "titl2" }, "inside h2");
+const container = React.createElement("div", { id: "container" }, [
+  heading1,
+  heading2,
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
